4332b5892c72ba9885c4d73420d6b4e558536a9a
Ira <dudairocka@gmail.com>
2024-10-13 18:56:08 +0700

    можно разобрать 7-10 на уроке пожалуйста

--- task2.py
+++ task2.py
@@ -3,12 +3,12 @@
 # и сколько других.
 
 n = int(input())
-a=0
-b=0
-while n !=0:
-    if n>9 and n<100:
-        a+=1
-    if n>0 and (n<10 or n>99):
-        b+=1
+a = 0
+b = 0
+while n != 0:
+    if n > 9 and n < 100:
+        a += 1
+    if n > 0 and (n < 10 or n > 99):
+        b += 1
     n = int(input())
-print(a,b)
+print(a, b)
